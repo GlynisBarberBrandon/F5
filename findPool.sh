ef71f850c78ab0fd07678aa4e954a9e3b463db7a
GlynisBarberBrandon <46563324+GlynisBarberBrandon@users.noreply.github.com>
2022-10-17 17:10:29 +0300

    Added some checks

--- findPool.sh
+++ findPool.sh
@@ -1,34 +1,41 @@
 #!/bin/bash 
 file="$1"
 poolName="$2"
+
+if [ $# -lt 2 ]; then
+    echo "Usage: findPool.sh  /Path/of/bigip.conf  PoolName"
+    exit 5
+fi
+
 start=$(grep -n -E "^ltm rule /" ${file} | head -n 1 | cut -d ":" -f1)
 lastRule=$(grep -n -E "^ltm rule /" ${file} | tail -n 1 | cut -d ":" -f1)
 end=$(grep -n -E "^ltm (.*) /" ${file} | grep -A1 "${lastRule}" | tail -n 1 | cut -d ":" -f 1 )
 end=$(( end -1 ))
 
 declare -a arry
-read -a arry <<< $( sed -n ${start},${end}p ${file} | grep -n -E "$poolName" | cut -d ":" -f1 | tr '\n' ' ') 
-if (( ${#arry[@]} )); then  
+read -a arry <<< $( sed -n ${start},${end}p ${file} | grep -n -E "$poolName" | cut -d ":" -f1 | tr '\n' ' ')
+if (( ${#arry[@]} )); then
 
-	declare -a rulesArry
-	read -a rulesArry <<< $( grep -n -E "^ltm rule /" ${file} | cut -d ":" -f1 | tr '\n' ' ' )
-	rulesArry+=("$end")
-	
-	for i in "${!arry[@]}"
-	do
-		arry[$i]=$(( ${arry[$i]} + $start ))
- 		for m in "${!rulesArry[@]}"
-		do
-			if (( ${arry[$i]} > ${rulesArry[$m]} )) && (( ${arry[$i]} < ${rulesArry[$m +1]} )); then
-				echo "Found    ${rulesArry[$m]} ${arry[$i]} ${rulesArry[$m +1]}"
-				sed -n "${rulesArry[$m]},${rulesArry[$m +1]}p" ${file} | head -n 1 | sed -e 's/ {$//g'
-			fi
-		done
+    declare -a rulesArry
+    read -a rulesArry <<< $( grep -n -E "^ltm rule /" ${file} | cut -d ":" -f1 | tr '\n' ' ' )
+    rulesArry+=("$end")
 
-	done
+    for i in "${!arry[@]}"
+    do
+        arry[$i]=$(( ${arry[$i]} + $start ))
+        for m in "${!rulesArry[@]}"
+        do
+            if (( ${arry[$i]} > ${rulesArry[$m]} )) && (( ${arry[$i]} < ${rulesArry[$m +1]} )); then
+                #echo "Found    ${rulesArry[$m]} ${arry[$i]} ${rulesArry[$m +1]}"
+                sed -n "${rulesArry[$m]},${rulesArry[$m +1]}p" ${file} | head -n 1 | sed -e 's/ {$//g'
+            fi
+        done
 
-else 
+    done
 
-	echo "No Match Found in iRules"
+else
+
+    echo "No Match Found in iRules"
 
 fi
+
