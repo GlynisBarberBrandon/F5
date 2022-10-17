ba165be35acbef6debb9d7381c8fe22074704d77
GlynisBarberBrandon <46563324+GlynisBarberBrandon@users.noreply.github.com>
2022-10-17 16:52:51 +0300

    Added findPool.sh
    findPool.sh: This scripts designed to find pools used in iRules. Basically you need two parameters. The first parameter is the path of "bigip.conf" and the second is the pool name you look for.

---
+++ findPool.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+file="$1"
+poolName="$2"
+start=$(grep -n -E "^ltm rule /" ${file} | head -n 1 | cut -d ":" -f1)
+lastRule=$(grep -n -E "^ltm rule /" ${file} | tail -n 1 | cut -d ":" -f1)
+end=$(grep -n -E "^ltm (.*) /" ${file} | grep -A1 "${lastRule}" | tail -n 1 | cut -d ":" -f 1 )
+end=$(( end -1 ))
+
+declare -a arry
+read -a arry <<< $( sed -n ${start},${end}p ${file} | grep -n -E "$poolName" | cut -d ":" -f1 | tr '\n' ' ') 
+if (( ${#arry[@]} )); then  
+
+	declare -a rulesArry
+	read -a rulesArry <<< $( grep -n -E "^ltm rule /" ${file} | cut -d ":" -f1 | tr '\n' ' ' )
+	rulesArry+=("$end")
+	
+	for i in "${!arry[@]}"
+	do
+		arry[$i]=$(( ${arry[$i]} + $start ))
+ 		for m in "${!rulesArry[@]}"
+		do
+			if (( ${arry[$i]} > ${rulesArry[$m]} )) && (( ${arry[$i]} < ${rulesArry[$m +1]} )); then
+				echo "Found    ${rulesArry[$m]} ${arry[$i]} ${rulesArry[$m +1]}"
+				sed -n "${rulesArry[$m]},${rulesArry[$m +1]}p" ${file} | head -n 1 | sed -e 's/ {$//g'
+			fi
+		done
+
+	done
+
+else 
+
+	echo "No Match Found in iRules"
+
+fi
